51462342eb4409682f792e6c48d9af9200011c90
Cristian Loera <loera_73@hotmail.com>
2024-07-15 16:03:37 -0600

    Created the Models

---
+++ models/index.js
@@ -0,0 +1,16 @@
+const User = require('./User');
+const Posts = require('./Posts');
+
+
+// Creates a relationship between User and Project model, with the User having a "has many" relationship with Project model.
+User.hasMany(Posts, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+// Creates a relationship between User and Project model, with a "belongs to" relationship of the Project to the User.
+Posts.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Posts };
