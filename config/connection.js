c327770f49f4cb1754d23c66170114cf1e22f995
Cristian Loera <loera_73@hotmail.com>
2024-07-20 16:24:41 -0600

    Changed connection.js

--- config/connection.js
+++ config/connection.js
@@ -13,7 +13,7 @@ if (process.env.DB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'localhost',
+      host: process.env.DB_HOST,
       dialect: 'postgres'
     }
   );
